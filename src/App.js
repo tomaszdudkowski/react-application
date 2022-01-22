78549cec3771651d385348f8cad71fa4c3153a41
tomaszdudkowski <tomaszszd@gmail.com>
2022-01-22 22:39:28 +0100

    Basic card template code.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import "./SCSS/App.scss";
 import Header from "./Components/header";
-// import Hero from "./Components/hero";
+import Hero from "./Components/hero";
 import Card from "./Components/card";
+import Welcome from "./Components/welcome";
 
 function App() {
   return (
     <div>
       <Header />
-      {/* <Hero /> */}
-      <Card />
+      <Hero />
+      <div className="wrapper">
+        <Welcome />
+        <Card 
+          img={require('./Image/Card_Image/card_1.jpg')}
+          rate="5.0"
+          city="Lodz"
+          cost="2300"
+        />
+        <Card 
+          img={require('./Image/Card_Image/card_2.jpg')}
+          rate="5.3"
+          city="Warsaw"
+          cost="4500"
+        />
+        <Card 
+          img={require('./Image/Card_Image/card_3.jpg')}
+          rate="4"
+          city="Wroclaw"
+          cost="1600"
+        />
+        <Card 
+          img={require('./Image/Card_Image/card_1.jpg')}
+          rate="3.5"
+          cost="1000"
+        />
+      </div>
     </div>
   );
 }
--- src/Components/card.js
+++ src/Components/card.js
@@ -1,22 +1,29 @@
-import React from "react";
 import "../SCSS/Components/_card.scss";
 import star from "../Image/Card_Image/star-icon-48.png";
 
-function Card() {
-    return(
-        <div className="card">
-          <img src={require('../Image/Card_Image/card_1.jpg')} alt="img_1" className="card--image" />
-          <div className="card--stats">
-              <img className="card--star" src={star} alt="star"/>
-              <span className="bold">5.0</span>
-              <span className="gray">(6)</span>
-              <span className="gray">@ Warsaw</span>
-          </div>
-          <p>Proffesionl photography course in Warsaw</p>
-          <p><span className="bold">From PLN4500</span> / person</p>
-        </div>
-        
-    );
+function Card(props) {
+  var city;
+  if (props.city == null) {
+    city = "Online";
+  } else {
+    city = props.city;
+  }
+
+  return (
+    <div className="card">
+      <img src={props.img} alt="img_" className="card--image" />
+      <div className="card--stats">
+        <img className="card--star" src={star} alt="star" />
+        <span className="bold">{props.rate}</span>
+        <span className="gray">(6)</span>
+        <span className={city === "Online" ? "red" : "gray"}>@ {city}</span>
+      </div>
+      <p>Proffesionl photography course in {city}</p>
+      <p>
+        <span className="bold">From PLN{props.cost}</span> / person
+      </p>
+    </div>
+  );
 }
 
 export default Card;
