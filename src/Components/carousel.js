32bcb28690e26af0bb698197ea2f3e03eba9e18c
tomaszdudkowski <tomaszszd@gmail.com>
2022-01-23 17:34:51 +0100

    Add hero-text area to carousel.

--- src/Components/carousel.js
+++ src/Components/carousel.js
@@ -1,4 +1,5 @@
 import "../SCSS/Components/_carousel.scss";
+import "../SCSS/Components/_hero.scss";
 import image1 from "../Image/taylor-vick-M5tzZtFCOfs-unsplash.jpg";
 import image2 from "../Image/tianyi-ma-WiONHd_zYI4-unsplash.jpg";
 import image3 from "../Image/donald-giannatti-Wj1D-qiOseE-unsplash.jpg";
@@ -98,12 +99,68 @@ function Carousel() {
       <div className="carousel__track-container">
         <ul className="carousel__track">
           <li className="carousel__slide current-slide">
+            <div className="hero">
+              <div className="text-center">
+                <p className="text-small">
+                  CLOUD&CYBERSECURITY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;JAN&nbsp;21,&nbsp;2022&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOMASZ&nbsp;DUDKOWSKI
+                </p>
+                <h1>
+                  <a href="#home">
+                    Rackspace Technology:
+                    <br /> Delivering multi-cloud solutions
+                  </a>
+                </h1>
+                <p>
+                  As digital transformation continues and AI becomes more
+                  accessible, we take a look at some of the top AI applications
+                  for business in 2022
+                </p>
+              </div>
+            </div>
+
             <img className="carousel__image" src={image1} alt=""></img>
           </li>
           <li className="carousel__slide">
+            <div className="hero">
+              <div className="text-center">
+                <p className="text-small">
+                  CLOUD&CYBERSECURITY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;JAN&nbsp;21,&nbsp;2022&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOMASZ&nbsp;DUDKOWSKI
+                </p>
+                <h1>
+                  <a href="#home">
+                    Rackspace Technology:
+                    <br /> Delivering multi-cloud solutions
+                  </a>
+                </h1>
+                <p>
+                  As digital transformation continues and AI becomes more
+                  accessible, we take a look at some of the top AI applications
+                  for business in 2022
+                </p>
+              </div>
+            </div>
+
             <img className="carousel__image" src={image2} alt=""></img>
           </li>
           <li className="carousel__slide">
+            <div className="hero">
+              <div className="text-center">
+                <p className="text-small">
+                  CLOUD&CYBERSECURITY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;JAN&nbsp;21,&nbsp;2022&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOMASZ&nbsp;DUDKOWSKI
+                </p>
+                <h1>
+                  <a href="#home">
+                    Rackspace Technology:
+                    <br /> Delivering multi-cloud solutions
+                  </a>
+                </h1>
+                <p>
+                  As digital transformation continues and AI becomes more
+                  accessible, we take a look at some of the top AI applications
+                  for business in 2022
+                </p>
+              </div>
+            </div>
             <img className="carousel__image" src={image3} alt=""></img>
           </li>
         </ul>
