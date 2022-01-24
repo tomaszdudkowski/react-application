f80fa84aea9363ad514efe2ba269b55db42ee217
tomaszszd <tomaszszd@gmail.com>
2022-01-24 12:39:50 +0100

    Add props for slide component.

--- src/Components/carousel.js
+++ src/Components/carousel.js
@@ -1,5 +1,5 @@
 import "../SCSS/Components/_carousel.scss";
-import "../SCSS/Components/_hero.scss";
+import Slide from "./slide";
 import image1 from "../Image/taylor-vick-M5tzZtFCOfs-unsplash.jpg";
 import image2 from "../Image/tianyi-ma-WiONHd_zYI4-unsplash.jpg";
 import image3 from "../Image/donald-giannatti-Wj1D-qiOseE-unsplash.jpg";
@@ -99,68 +99,28 @@ function Carousel() {
       <div className="carousel__track-container">
         <ul className="carousel__track">
           <li className="carousel__slide current-slide">
-            <div className="hero">
-              <div className="text-center">
-                <p className="text-small">
-                  CLOUD&CYBERSECURITY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;JAN&nbsp;21,&nbsp;2022&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOMASZ&nbsp;DUDKOWSKI
-                </p>
-                <h1>
-                  <a href="#home">
-                    Rackspace Technology:
-                    <br /> Delivering multi-cloud solutions
-                  </a>
-                </h1>
-                <p>
-                  As digital transformation continues and AI becomes more
-                  accessible, we take a look at some of the top AI applications
-                  for business in 2022
-                </p>
-              </div>
-            </div>
-
+            <Slide
+              tags="CLOUD & CYBERSECURITY"
+              title="Rackspace Technology: Delivering multi-cloud solutions"
+              desc=""
+            />
             <img className="carousel__image" src={image1} alt=""></img>
           </li>
           <li className="carousel__slide">
-            <div className="hero">
-              <div className="text-center">
-                <p className="text-small">
-                  CLOUD&CYBERSECURITY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;JAN&nbsp;21,&nbsp;2022&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOMASZ&nbsp;DUDKOWSKI
-                </p>
-                <h1>
-                  <a href="#home">
-                    Rackspace Technology:
-                    <br /> Delivering multi-cloud solutions
-                  </a>
-                </h1>
-                <p>
-                  As digital transformation continues and AI becomes more
-                  accessible, we take a look at some of the top AI applications
-                  for business in 2022
-                </p>
-              </div>
-            </div>
-
+            <Slide
+              tags="AI & MACHINE LEARNING"
+              title="The Top AI Application for Business in 2022"
+              desc="As digital transformation continues and AI becomes more accessible,
+              we take a look at some of the top AI applications for business in 2022"
+            />
             <img className="carousel__image" src={image2} alt=""></img>
           </li>
           <li className="carousel__slide">
-            <div className="hero">
-              <div className="text-center">
-                <p className="text-small">
-                  CLOUD&CYBERSECURITY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;JAN&nbsp;21,&nbsp;2022&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOMASZ&nbsp;DUDKOWSKI
-                </p>
-                <h1>
-                  <a href="#home">
-                    Rackspace Technology:
-                    <br /> Delivering multi-cloud solutions
-                  </a>
-                </h1>
-                <p>
-                  As digital transformation continues and AI becomes more
-                  accessible, we take a look at some of the top AI applications
-                  for business in 2022
-                </p>
-              </div>
-            </div>
+          <Slide
+              tags="DATA & DATA ANALYTICS"
+              title="Data Analytics: 2022 Trends and Opportunities"
+              desc=""
+            />
             <img className="carousel__image" src={image3} alt=""></img>
           </li>
         </ul>
---
+++ src/Components/slide.js
@@ -0,0 +1,58 @@
+import "../SCSS/Components/_hero.scss";
+
+function Slide(props) {
+  const date = new Date();
+  const year = date.getFullYear();
+  const monthNames = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
+  ];
+  const month = monthNames[date.getMonth()];
+
+  const day = date.toLocaleDateString("us-US", {
+    day: "2-digit"
+  });
+
+  return (
+    <div>
+      <div className="hero">
+        <div className="text-center">
+          <p className="text-small">
+            <span>
+              {props.tags}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{month}
+              &nbsp;{day}
+              &nbsp;{year}
+            </span>
+          </p>
+          <h1>
+            <a href="#home">{props.title}</a>
+          </h1>
+          <p>{props.desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Slide;
+
+// CLOUD&CYBERSECURITY;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;JAN&nbsp;21,&nbsp;2022&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOMASZ&nbsp;DUDKOWSKI
+
+// Rackspace Technology:
+//   <br /> Delivering multi-cloud solutions
+
+// As digital transformation continues and AI becomes more accessible,
+// we take a look at some of the top AI applications for business in
+// 2022
