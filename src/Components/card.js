134f24ed09b81cba6cc823ba1b2cbebcf267b58b
tomaszdudkowski <tomaszszd@gmail.com>
2022-01-27 21:14:08 +0100

    Add aplication card map from data.js

--- src/Components/card.js
+++ src/Components/card.js
@@ -2,13 +2,6 @@ import "../SCSS/Components/_card.scss";
 import star from "../Image/Card_Image/star-icon-48.png";
 
 function Card(props) {
-  var city;
-  if (props.city == null) {
-    city = "Online";
-  } else {
-    city = props.city;
-  }
-
   return (
     <div className="card">
       <img src={props.img} alt="img_" className="card--image" />
@@ -16,7 +9,7 @@ function Card(props) {
         <img className="card--star" src={star} alt="star" />
         <span className="bold">{props.rate}</span>
         <span className="gray">(6)</span>
-        <span className={city === "Online" ? "red" : "gray"}>@ {city}</span>
+        <span >{city}</span>
       </div>
       <p>Proffesionl photography course in {city}</p>
       <p>
---
+++ src/data.js
@@ -0,0 +1,94 @@
+let data = [
+  {
+    id: 1,
+    categories: "Digital Transformation",
+    title: "Software: The key for progression in the e-mobility sector",
+    author: "Freddie Eastham",
+    date: new Date(2022, 1, 23),
+    readTime: 5,
+    description:
+      "Freddie Eastham explains why software, not hardware, will be the biggest enabler for progression in the e-mobility sector",
+    coverImage: "../",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+  },
+  {
+    id: 2,
+    categories: "Digital Transformation",
+    title: "Wisk: Working to deliver autonomous, all-electric flights",
+    author: "Tilly Kenyon",
+    date: new Date(2022, 1, 27),
+    readTime: 5,
+    description:
+      "Wisk, developer of the first all-electric, self-flying air taxi in the US, has secured $450 million in funding from The Boeing Company",
+    coverImage: "../",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+  },
+  {
+    id: 3,
+    categories: "AI & Machine Learning",
+    title: "European deeptech drone firm Tekever given lift",
+    author: "Tilly Kenyon",
+    date: new Date(2022, 1, 26),
+    readTime: 4,
+    description:
+      "Revolutionary drone-based Intelligence-as-a-Service firm set for global expansion with 20 million euro cash boost from Ventura Capital",
+    coverImage: "../",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+  },
+  {
+    id: 4,
+    categories: "Digital Transformation",
+    title: "Wisk: Working to deliver autonomous, all-electric flights",
+    author: "Tom Swallow",
+    date: new Date(2022, 1, 27),
+    readTime: 2,
+    description:
+      "Wisk, developer of the first all-electric, self-flying air taxi in the US, has secured $450 million in funding from The Boeing Company",
+    coverImage: "../",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+  },
+  {
+    id: 5,
+    categories: "Cloud & Cybersecurity",
+    title: "Rackspace Technology: Delivering multi-cloud solutions",
+    author: "Tilly Kenyon",
+    date: new Date(2022, 1, 20),
+    readTime: 3,
+    description:
+      "Revolutionary drone-based Intelligence-as-a-Service firm set for global expansion with 20 million euro cash boost from Ventura Capital",
+    coverImage: "../",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+  },
+  {
+    id: 6,
+    categories: "Enterprise IT",
+    title: "The SAP solution to business sustainability monitoring",
+    author: "Tom Swallow",
+    date: new Date(2022, 1, 23),
+    readTime: 4,
+    description:
+      "Revolutionary drone-based Intelligence-as-a-Service firm set for global expansion with 20 million euro cash boost from Ventura Capital",
+    coverImage: "../",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+  },
+];
+
+export default data;
