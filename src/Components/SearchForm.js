71066f16cdd26d4d18506ee4c3b5c0ac57a3705e
tomaszszd <tomaszszd@gmail.com>
2022-03-03 09:33:17 +0100

    Add search logic for Message Hook.

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
+import { useMessages } from "../Hooks/MessageHooks";
 
 export default function SearchForm() {
   const [phrase, setPhrase] = useState("");
 
+  const { searchMessage } = useMessages();
+
   return (
     <>
-      <h1>{phrase}</h1>
       <form>
         <input
           value={phrase}
-          onChange={(event) => setPhrase(event.target.value)}
+          onChange={(event) => {
+            setPhrase(event.target.value);
+            searchMessage(phrase);
+          }}
           type="text"
           placeholder="Search phrase"
           required
--- src/Hooks/MessageHooks.js
+++ src/Hooks/MessageHooks.js
@@ -8,8 +8,9 @@ export function MessageProvider ({ children }) {
     const [messages, setMessages] = useState(messagesData);
 
     const removeMessage = id => setMessages(messages.filter(message => message.id !== id))
+    const searchMessage = text => setMessages(messages.filter(message => message.message.includes(text) === true || message.title.includes(text)))
     return(
-        <MessageContext.Provider value={{messages, removeMessage}}>
+        <MessageContext.Provider value={{messages, removeMessage, searchMessage}}>
             {children}
         </MessageContext.Provider>
     )
