9a43ec872766083ebe714a0037a7198d05e028eb
tomaszdudkowski <tomaszszd@gmail.com>
2022-03-22 21:52:50 +0100

    Add style for GitRepo component

--- src/Components/Elements/_GitRepo.js
+++ src/Components/Elements/_GitRepo.js
@@ -1,8 +1,12 @@
+import "../../SCSS/Components/Elements/_gitRepo.scss";
+import "../../SCSS/Layout/_flex.scss";
+import "../../SCSS/Layout/_column.scss";
+
 export default function GitRepo({ name, html_url }) {
     return (
-        <div>
-            <h1>{name}</h1>
-            <a href={html_url}>Link: {name}</a>
+        <div className="repo">
+            <h1 className="repo-name">{name}</h1>
+            <a className="repo-url" href={html_url}>Link to repo</a>
         </div>
     )
 }
--- src/Components/Elements/_GitRepos.js
+++ src/Components/Elements/_GitRepos.js
@@ -2,14 +2,18 @@ import { useGit } from "../../Hooks/GitHooks";
 import GitRepo from "./_GitRepo";
 
 export default function GitRepos() {
-  const { repoData, repoLoading } = useGit();
+  const { userData, repoData, repoLoading } = useGit();
 
   let RepoItem;
   if (repoData && !repoLoading) {
-    RepoItem = repoData.map((data) => {
-      return <GitRepo key={data.id} {...data}/>
-    });
+    if (userData.login === repoData[0].owner.login) {
+      RepoItem = repoData.map((data) => {
+        return <GitRepo key={data.id} {...data} />;
+      });
+    } else {
+      RepoItem = null
+    }
   }
 
-  return ( <div> {RepoItem} </div>)
+  return <div> {RepoItem} </div>;
 }
