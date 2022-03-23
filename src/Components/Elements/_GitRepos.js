abaae977d27c9a2d9da138c270d5c6b2b4eb941a
tomaszszd <tomaszszd@gmail.com>
2022-03-23 12:01:52 +0100

    Fir GitRepos component

--- src/Components/Elements/_GitRepos.js
+++ src/Components/Elements/_GitRepos.js
@@ -2,16 +2,18 @@ import { useGit } from "../../Hooks/GitHooks";
 import GitRepo from "./_GitRepo";
 
 export default function GitRepos() {
-  const { userData, repoData, repoLoading } = useGit();
+  const { repoData, userData, userLoading, repoLoading } = useGit();
 
   let RepoItem;
   if (repoData && !repoLoading) {
-    if (userData.login === repoData[0].owner.login) {
-      RepoItem = repoData.map((data) => {
-        return <GitRepo key={data.id} {...data} />;
-      });
-    } else {
-      RepoItem = null
+    if (userData && !userLoading) {
+      if (repoData[0].owner.login === userData.login) {
+        RepoItem = repoData.map((data) => {
+          return <GitRepo key={data.id} {...data} />;
+        });
+      } else {
+        return <></>
+      }
     }
   }
 
--- src/Hooks/GitHooks.js
+++ src/Hooks/GitHooks.js
@@ -5,16 +5,41 @@ const GitContext = createContext();
 export const useGit = () => useContext(GitContext);
 
 export function GitProvider({ children }) {
-  const {data : userData, error : userError, loading : userLoading, fetchData : userFetch} = useFetch("")
-  const {data: repoData, error : repoError, loading : repoLoading, fetchData : repoFetch} = useFetch("")
+  const {
+    data: userData,
+    error: userError,
+    loading: userLoading,
+    fetchData: userFetch,
+  } = useFetch();
+  const {
+    data: repoData,
+    error: repoError,
+    loading: repoLoading,
+    fetchData: repoFetch,
+  } = useFetch();
 
-  const fetchUser = (user) => {
-    userFetch(`https://api.github.com/users/${user}`)
-  }
+  const fetchUser = (user = "tomaszdudkowski") => {
+    userFetch(`https://api.github.com/users/${user}`);
+  };
 
-  const fetchRepos = (login) => {
-    repoFetch(`https://api.github.com/users/${login}/repos`)
-  }
+  const fetchRepos = (login = "tomaszdudkowski") => {
+    repoFetch(`https://api.github.com/users/${login}/repos`);
+  };
 
-  return <GitContext.Provider value={{ userData, userError, userLoading, repoData, repoError, repoLoading, fetchUser, fetchRepos }}>{children}</GitContext.Provider>;
+  return (
+    <GitContext.Provider
+      value={{
+        userData,
+        userError,
+        userLoading,
+        repoData,
+        repoError,
+        repoLoading,
+        fetchUser,
+        fetchRepos
+      }}
+    >
+      {children}
+    </GitContext.Provider>
+  );
 }
